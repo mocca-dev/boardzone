75077262caafeb4895001ed739fd5c0e008907fa
mocca-dev <nicolastonelli1406@gmail.com>
2021-08-11 14:58:19 -0300

    Skull png relocation

--- src/components/SkullIcon/SkullIcon.tsx
+++ src/components/SkullIcon/SkullIcon.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import style from './SkullIcon.module.css';
-import skull from './../skull.png';
+import skull from './skull.png';
 
 interface ISkullIcon {
   isYou: boolean;
